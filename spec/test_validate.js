479fa595c9c87a886b7b1283be97c585157ef5a8
Luca Greco <lgreco@mozilla.com>
2021-05-05 13:46:44 +0200

    fix: Updated changes to restrict updated version to ajv v8

--- spec/test_validate.js
+++ spec/test_validate.js
@@ -5,25 +5,15 @@ var assert = require('assert');
 module.exports = function (validate, keyword) {
   assert.strictEqual(validate({ p: 'abc', q: 1 }), true);
 
-  // In ajv <= v7 the property is dataPath, while on ajv >= v8 is instancePath.
-  var assertDataOrInstancePath = function (err, expectedValue) {
-    assert.equal(
-      'dataPath' in err
-        ? err.dataPath
-        : err.instancePath,
-      expectedValue
-    );
-  };
-
   // property q should be a number
   assert.strictEqual(validate({ p: 'foo', q: 'bar' }), false);
   var errs = validate.errors;
   assert.equal(errs.length, 2);
   assert.equal(errs[0].keyword, 'type');
   assert.equal(errs[0].schemaPath, '#/' + keyword + '/properties/q/type');
-  assertDataOrInstancePath(errs[0], '/q');
+  assert.equal(errs[0].instancePath, '/q');
   assert.equal(errs[1].keyword, keyword);
-  assertDataOrInstancePath(errs[1], '');
+  assert.equal(errs[1].instancePath, '');
   assert.equal(errs[1].schemaPath, '#/' + keyword);
 
   // an object without q should fail
@@ -33,8 +23,8 @@ module.exports = function (validate, keyword) {
   assert.equal(errs[0].keyword, 'required');
   assert.equal(errs[0].schemaPath, '#/' + keyword + '/required');
   assert.deepEqual(errs[0].params, { missingProperty: 'q' });
-  assertDataOrInstancePath(errs[0], '');
+  assert.equal(errs[0].instancePath, '');
   assert.equal(errs[1].keyword, keyword);
   assert.equal(errs[1].schemaPath, '#/' + keyword);
-  assertDataOrInstancePath(errs[1], '');
+  assert.equal(errs[1].instancePath, '');
 };
